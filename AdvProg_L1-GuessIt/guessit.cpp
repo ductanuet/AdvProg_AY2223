bccb0ef090b9eca4f7392262fd1e645464ea029a
ductanuet <vuductantb123@gmail.com>
2023-04-03 21:26:48 +0700

    22026543

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -5,98 +5,56 @@
 
 using namespace std;
 
-/***
-    Args:
-        
-    Returns:
-        number (int) : random number in range 1-100
-***/
 int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
-    srand(time(NULL));
-    int secretNumber = rand() % 100 + 1;
-    return secretNumber;
+    srand(time(NULL)) ;
+    int randomNumber = rand() % 100 + 1 ;
+    return randomNumber;
 }
 
-
-/***
-    Args:
-        
-    Returns:
-        number (int) : the number that player guessed
-***/
 int getPlayerGuess() {
-    int guess;
-    cout<<"your the guess";
-    cin>>guess;
-    return guess;
+    cout << "PlayerGuess : " ;
+    int x ; cin >> x ;
+    return x ;
 }
 
-
-/***
-    Args:
-        number (int): answer number from player
-        randomNumber (int): the true number
-    Returns:
-        answer (string) : answer of computer after checking result
-***/
 string getAnswer(int number, int randomNumber) {
-    /*** 
-        TODO: check number with randomNumber and return the result.
-              If number is higher than randomNumber, the answer is "Your number is higher."
-              If number is lower than randomNumber, the answer is "Your number is lower."
-              If number is equal randomNumber, the answer is "Congratulation! You win."
-    ***/
     string answer;
-    if(number > randomNumber) answer = "Your number is higher.";
-    else if(number < randomNumber) answer = "Your number is lower.";
-    else answer = "Congratulation! You win.";
+    if(number > randomNumber)
+    {
+        answer = "Your number is higher." ;
+    }
+    else if(number < randomNumber)
+    {
+        answer = "Your number is lower." ;
+    }
+    else
+    {
+        answer = "Congratulation! You win." ;
+    }
     return answer;
 }
 
-
-/***
-    Args:
-        answer (string): answer from computer after compare numbers
-    Returns:
-        result (bool) : player win or not
-***/
 bool checkSuccess(string answer) {
-    // TODO: return the result after checking that player guessed right or wrong
-    if(answer == " Congratulation! You win.")
-    return true;
-    else return false;
+    if(answer == "Congratulation! You win.")
+    {
+        return true ;
+    }
+    else
+    {
+        return false ;
+    }
 }
 
-
-/***
-    Args:
-        isContinued (char): player's choice
-    Returns:
-        result (bool) : continue playing or not
-***/
 bool checkContinuePlaying(char isContinued) {
-    // TODO: return result after checking player continue playing or not
-    bool result = false;
-    if(isContinued == 'Y' || isContinued == 'y') result = true;
-    return result;
+    return (isContinued == 'Y' || isContinued == 'y') ;
 }
 
-
-/***
-    Args:
-        
-    Returns:
-        isContinues (char) : player's choice (continue playing or not)
-***/
 char getPlayerOpinion() {
-    // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-    cout<<"Do you want to continue playing?(y/n)"<<endl;
-    cin>>isContinued;
-    return isContinued;
+    cout << "Continue playing ?(Y/N)\n" ;
+    cin >> isContinued ;
+    return isContinued ;
 }
-
 
 void playGuessIt() {
     int randomNumber = generateRandomNumber();
--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -17,7 +17,8 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
-    return 1;
+    //srand(time(0));
+    return rand()%(max - min) + min;
 }
 
 vector<string> readWordListFromFile(const string& filePath)
@@ -51,7 +52,10 @@ vector<string> readWordListFromFile(const string& filePath)
 bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
-    return true;
+    for(int i=0;i<(int)word.size();i++){
+        if(ch == word[i]) return true;
+    }
+    return false;
 }
 
 /***
@@ -65,7 +69,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
-
+    answer = wordList[index-1];
     return answer;
 }
 
@@ -78,7 +82,9 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
-
+    for(int i=0;i<(int)answerWord.size();i++){
+        secretWord += "-";
+    }
     return secretWord;
 }
 
@@ -99,6 +105,9 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
+    for(int i=0;i<(int)word.size();i++){
+        if(ch == word[i]) secretWord[i] = word[i];
+    }
 }
 
 /***
@@ -110,6 +119,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
+    chars += ch + " ";
 }
 
 /***
@@ -120,6 +130,7 @@ void updateEnteredChars(const char ch, string& chars){
 ***/
 void updateIncorrectGuess(int& incorrectGuess){
     // TODO: increase the value of incorrectGuess by 1
+    incorrectGuess++;
 }
 
 /***
